7c7d63ecf9bde336f21a763763fcdfda182cc4ae
QPvicus <57519939+QPvicus@users.noreply.github.com>
2020-06-24 20:14:56 +0800

    频道列表完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -13,23 +13,70 @@
         搜索</van-button
       >
     </van-nav-bar>
+    <!-- 文章频道列表 -->
+    <van-tabs v-model="active" class="channel-tabs">
+      <van-tab :title="item.name" v-for="item in channels" :key="item.id"
+        >{{ item.name }} 的详细内容</van-tab
+      >
+    </van-tabs>
   </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/user'
 export default {
   name: 'HomeIndex',
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      active: 0, // 控制激活的标签
+      channels: [] // 频道列表
+    }
   },
   computed: {},
   watch: {},
-  created() {},
+  created() {
+    this.loadChannels()
+  },
   mounted() {},
-  methods: {}
+  methods: {
+    async loadChannels() {
+      const { data } = await getUserChannels()
+      this.channels = data.data.channels
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: none;
+  }
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background: #5babfb;
+    border: none;
+    .van-icon {
+      font-size: 16px;
+    }
+    .van-button__text {
+      font-size: 14px;
+    }
+  }
+  .channel-tabs {
+    /deep/ .van-tab {
+      border-right: 1px solid #edeff3;
+      border-bottom: 1px solid #edeff3;
+    }
+    /deep/ .van-tabs__line {
+      bottom: 20px;
+      width: 15px !important;
+      height: 3px;
+      background: #3296fa;
+    }
+  }
+}
+</style>
